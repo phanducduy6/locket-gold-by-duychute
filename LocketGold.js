61e742f34610ab80d85c453b0dac4f0717a94610
phanducduy6 <uchihaduczi@gmail.com>
2025-12-21 15:18:31 +0700

    Add LocketGold.js for subscription handling

---
+++ LocketGold.js
@@ -0,0 +1,41 @@
+const mapping = {
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'Locket': ['Gold']
+};
+
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+var pattern = /^Locket/;
+
+if (pattern.test(ua)) {
+  var body = JSON.parse($response.body);
+  var obj = body.subscriber;
+  var data = {
+    "expires_date": "2099-09-09T09:09:09Z",
+    "original_purchase_date": "2023-09-09T09:09:09Z",
+    "purchase_date": "2023-09-09T09:09:09Z",
+    "ownership_type": "PURCHASED",
+    "store": "app_store"
+  };
+  
+  // Kích hoạt entitlements
+  obj.entitlements = {
+    "Gold": Object.assign({}, data, {
+      "product_identifier": "com.locket.gold.yearly"
+    })
+  };
+  
+  // Kích hoạt subscriptions
+  obj.subscriptions = {
+    "com.locket.gold.yearly": Object.assign({}, data, {
+      "is_sandbox": false,
+      "period_type": "normal",
+      "billing_issues_detected_at": null,
+      "unsubscribe_detected_at": null,
+      "expires_date": "2099-09-09T09:09:09Z"
+    })
+  };
+
+  $done({body: JSON.stringify(body)});
+} else {
+  $done({});
+}
